fa7ec152478dd119ddba7cb7177c744e93c6d364
yutaro okuda <yutaro.okuda.76@gmail.com>
2024-10-02 20:49:47 +0800

    page transitions

--- frontend/task_management/src/App.tsx
+++ frontend/task_management/src/App.tsx
@@ -1,18 +1,32 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import './App.css'
+
+import Home from "./components/Home";
+import User from "./components/User";
+import Project from "./components/Project";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
-    <>
-      <nav>
-        <div className="logo">
-          <h4>Project Management</h4>
+      <BrowserRouter>
+        <nav>
+          <div className="logo">
+            <Link to="/">Project Management</Link>
+          </div>
+          <ul className="nav-links">
+            <Link to="/user">User</Link>
+            <Link to="/project">Project</Link>
+          </ul>
+        </nav>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/user" element={<User />} />
+            <Route path="/project" element={<Project />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
         </div>
-        <ul className="nav-links">
-          <li><a href="#">User</a></li>
-          <li><a href="#">Project</a></li>
-        </ul>
-      </nav>
-    </>
+      </BrowserRouter>
   )
 }
 
