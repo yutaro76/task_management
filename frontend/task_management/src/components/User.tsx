ec698e131f5fd3e07f364d4775cf971787e93eac
yutaro okuda <yutaro.okuda.76@gmail.com>
2024-10-03 20:36:50 +0800

    display users

--- frontend/task_management/src/components/User.tsx
+++ frontend/task_management/src/components/User.tsx
@@ -1,7 +1,51 @@
+import { useState } from 'react';
+
+type User = {
+    value: string;
+  }
+
 const User = () => {
+
+  const [text, setText] = useState('');
+  const [users, setUsers] = useState<User[]>([]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  }
+
+  const handleSubmit = () => {
+    if (!text) return;
+
+    const newUser: User = {
+      value: text,
+    }
+
+    setUsers((users) => [newUser, ...users])
+
+    setText('');
+  }
+
   return (
     <div>
-      <h1>User</h1>
+      <form 
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <input 
+          type="text" 
+          value ={text} 
+          onChange={(e) => handleChange(e)} />
+        <input
+          type="submit"
+          value="追加"
+          onSubmit={handleSubmit}
+        />
+      </form>
+      <ul>
+        {users.map((user) => {return <li>{user.value}</li>;})}
+      </ul>
     </div>
   );
 };
