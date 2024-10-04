8f836c0ab68ec8ecf86a9a7836fae21eee88a34d
yutaro okuda <yutaro.okuda.76@gmail.com>
2024-10-04 20:28:30 +0800

    edit and delete user

--- frontend/task_management/src/components/User.tsx
+++ frontend/task_management/src/components/User.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 
 type User = {
     value: string;
+    readonly id: number;
+    removed: boolean;
   }
 
 const User = () => {
@@ -18,12 +20,40 @@ const User = () => {
 
     const newUser: User = {
       value: text,
+      id: new Date().getTime(),
+      removed: false,
     }
 
     setUsers((users) => [newUser, ...users])
 
     setText('');
   }
+
+  const handleEdit = (id: number, value: string) => {
+    setUsers((users) => {
+      const newUsers = users.map((user) => {
+        if (user.id === id) {
+          return {...user, value};
+        }
+        return user;
+      })
+      return newUsers;
+    });  
+  }
+
+  const handleRemove = (id: number, removed: boolean) => {
+    setUsers((users) => {
+      const newUsers = users.map((user) => {
+        if (user.id === id) {
+          return {...user, removed};
+        }
+        return user;
+      });
+      
+      const filteredRemovedUsers = newUsers.filter((user) => !user.removed);
+      return filteredRemovedUsers;
+    })
+  };
 
   return (
     <div>
@@ -44,7 +74,18 @@ const User = () => {
         />
       </form>
       <ul>
-        {users.map((user) => {return <li>{user.value}</li>;})}
+        {users.map((user) => {
+          return (
+            <li key={user.id}>
+              <input 
+                type="text"
+                value={user.value}
+                onChange={(e) => handleEdit(user.id, e.target.value)}
+              />
+              <button className="deleteButton" onClick={() => handleRemove(user.id, !user.removed)}>削除</button>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
